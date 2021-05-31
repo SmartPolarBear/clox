317093cc87bee2f51c7aef127cf8a4cf0cde89a0
SmartPolarBear <CleverCoolBear@outlook.com>
2021-05-31 23:03:41 +0800

    feat(parser): expression constructor generation
    feat(parser): expression getter/setter generation
    
    feat(parser): expression member definition generation

--- parser/generator/parser_gen.py
+++ parser/generator/parser_gen.py
@@ -1,9 +1,101 @@
 import argparse
 import pathlib
+import json
+
+JSON_EXPR_CLASSES_NAME: str = "expression_classes"
 
 
-def parse_config(arg_parser: argparse) -> list:
-    pass
+class ExpressionClassMemberDef:
+    __type: str
+    __name: str
+
+    def __init__(self, _name: str, _type: str):
+        __name = _name
+        __type = _type
+
+    def get_definition(self) -> str:
+        return "{0} {1}{{}};".format(self.__type, self.__name)
+        pass
+
+    def get_getter(self):
+        yield "[[nodiscard]] get_{}(){".format(self.__name[:-1])
+        yield "return this->{};}".format(self.__name)
+        pass
+
+    def get_setter(self):
+        yield "void set_{0}(const {1}&{0}){".format(self.__name[:-1], self.__type)
+        yield "{}={};}".format(self.__name, self.__name[:-1])
+
+
+class ExpressionClassConstructorDef:
+    __def: str
+
+    def __init__(self, __name: str, members_: dict):
+        self.__def = "[[nodiscard]] explicit {0}(".format(__name)
+
+        for m in members_:
+            self.__def += "{0} {1},".format(m["type"], str(m["name"])[:-1])
+
+        self.__def = self.__def[:-1]
+
+        self.__def += "):"
+
+        for m in members_:
+            self.__def += "{0}({1}),".format(m["name"], str(m["name"])[:-1])
+
+        self.__def = self.__def[:-1]
+
+        self.__def += "{{}}"
+
+    def get_definition(self) -> str:
+        return self.__def
+
+
+class ExpressionClass:
+    __name: str
+    __members: list[ExpressionClassMemberDef]
+
+    __ctor: ExpressionClassConstructorDef
+
+    def __init__(self, _name: str, members_: dict):
+        __name = _name
+        __members = list(ExpressionClassMemberDef(m["name"], m["type"]) for m in members_)
+
+    def lines(self):
+        yield "class {0}{{".format(self.__name)
+
+        yield "public:"
+
+        yield "{0}(const {0}&)=default;".format(self.__name)
+        yield "{0}({0}&&)=default;".format(self.__name)
+        yield "{0}& operator=(const {0}&)=default;".format(self.__name)
+
+        yield self.__ctor.get_definition()
+
+        for m in self.__members:
+            for s in m.get_getter():
+                yield s
+
+        for m in self.__members:
+            for s in m.get_setter():
+                yield s
+
+        yield "private:"
+
+        for m in self.__members:
+            yield m.get_definition()
+
+        yield "}};"
+
+
+def expression_classes(arg_parser: argparse):
+    with open(str(arg_parser.config[0]), "r") as conf:
+        conf_dict: dict = json.load(conf)
+
+        expr_classes: dict = conf_dict[JSON_EXPR_CLASSES_NAME]
+
+        for cls in ((ExpressionClass(expr_cls_def["name"], expr_cls_def["member"])) for expr_cls_def in expr_classes):
+            yield cls
 
 
 def write_back(arg_parser: argparse, head: list, content: list, tail: list):
@@ -24,13 +116,17 @@ def generate(arg_parser: argparse):
             if not line:
                 raise ValueError("template file is invalid")
 
-    tail: list = list()
-    line = template.readline()
-    while line.strip() != "!":
-        tail.append(line)
+        tail: list = list()
         line = template.readline()
+        while line.strip() != "!":
+            tail.append(line)
+            line = template.readline()
 
-    content: list = parse_config(arg_parser)
+    content: list[str] = list()
+
+    for cls in expression_classes(arg_parser):
+        for line in cls.lines():
+            content.append(line)
 
     write_back(arg_parser, head, content, tail)
 
