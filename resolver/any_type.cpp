646eb52bbbc8727f70d50c53424942c7916ab029
SmartPolarBear <CleverCoolBear@outlook.com>
2021-08-30 15:01:55 +0800

    feat(resolver): better unify() implementation

---
+++ resolver/any_type.cpp
@@ -0,0 +1,69 @@
+// Copyright (c) 2021 SmartPolarBear
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+//
+// Created by cleve on 8/30/2021.
+//
+
+#include <helper/enum.h>
+
+#include <resolver/lox_type.h>
+#include <resolver/instance_type.h>
+
+#include <tuple>
+#include <utility>
+#include <format>
+
+using namespace clox;
+
+using namespace clox::helper;
+
+using namespace clox::resolving;
+
+uint64_t lox_any_type::flags() const
+{
+	return TYPE_PRIMITIVE;
+}
+
+type_id lox_any_type::id() const
+{
+	return PRIMITIVE_TYPE_ID_ANY;
+}
+
+bool lox_any_type::operator<(const lox_type& target) const
+{
+	return true;
+}
+
+std::string lox_any_type::printable_string()
+{
+	return "<any type>";
+}
+
+bool lox_any_type::operator==(const lox_type& t) const
+{
+	return true;
+}
+
+bool lox_any_type::operator!=(const lox_type& lox_type) const
+{
+	return false;
+}
+
--- resolver/lox_type.cpp
+++ resolver/lox_type.cpp
@@ -2,6 +2,7 @@
 #include <helper/enum.h>
 
 #include <resolver/lox_type.h>
+#include <resolver/instance_type.h>
 
 #include <tuple>
 #include <utility>
@@ -13,62 +14,76 @@ using namespace clox::helper;
 
 using namespace clox::resolving;
 
-uint64_t lox_any_type::flags() const
+
+bool lox_type::unify(const lox_type& base, const lox_type& derived)
 {
-	return TYPE_PRIMITIVE;
+	if (lox_type::is_instance(base))
+	{
+		return unify(*dynamic_cast<const lox_instance_type&>(base).underlying_type(), derived);
+	}
+	if (lox_type::is_instance(derived))
+	{
+		return unify(base, *dynamic_cast<const lox_instance_type&>(derived).underlying_type());
+	}
+
+	if (base.id() == PRIMITIVE_TYPE_ID_ANY)
+	{
+		return true;
+	}
+
+	if (base.id() == PRIMITIVE_TYPE_ID_VOID)
+	{
+		return false;
+	}
+
+//	if (base.id() == TYPE_ID_INITIALIZER_LIST)
+//	{
+//		return false;
+//	}
+
+	return derived < base;
+
+//	if (derived.id() == TYPE_ID_INITIALIZER_LIST)
+//	{
+//		return dynamic_cast<const initializer_list_type&>(derived) < dynamic_cast<const lox_object_type&>(base);
+//	}
+//
+//
+//	return dynamic_cast<const lox_object_type&>(derived) < dynamic_cast<const lox_object_type&>(base);
 }
 
-type_id lox_any_type::id() const
+std::shared_ptr<lox_type> lox_type::intersect(const std::shared_ptr<lox_type>& t1, const std::shared_ptr<lox_type>& t2)
 {
-	return PRIMITIVE_TYPE_ID_ANY;
+	if (t1->id() == PRIMITIVE_TYPE_ID_ANY || t2->id() == PRIMITIVE_TYPE_ID_ANY)return std::make_shared<lox_any_type>();
+
+	if (is_primitive(*t1) && is_primitive(*t2))
+	{
+		return t1->id() >= t2->id() ? t1 : t2;
+	}
+
+	if (*t1 < *t2)return t2;
+
+	if (*t2 < *t1)return t1;
+
+	auto o1 = std::static_pointer_cast<lox_object_type>(t1), o2 = std::static_pointer_cast<lox_object_type>(t2);
+	if (o1->depth() < o2->depth())std::swap(o1, o2);
+
+	while (o1->depth() <= o1->depth())
+	{
+		o1 = o1->super();
+	}
+
+	while (o1 && o2 && *o1 != *o2)
+	{
+		o1 = o1->super();
+		o2 = o2->super();
+	}
+
+	if (o1 == nullptr || o2 == nullptr)
+	{
+		return nullptr;
+	}
+
+	return o1;
 }
 
-bool lox_any_type::operator<(const lox_type& target) const
-{
-	return true;
-}
-
-std::string lox_any_type::printable_string()
-{
-	return "<any type>";
-}
-
-bool lox_any_type::operator==(const lox_type& t) const
-{
-	return true;
-}
-
-bool lox_any_type::operator!=(const lox_type& lox_type) const
-{
-	return false;
-}
-
-std::string lox_void_type::printable_string()
-{
-	return "<void>";
-}
-
-uint64_t lox_void_type::flags() const
-{
-	return TYPE_VOID;
-}
-
-type_id lox_void_type::id() const
-{
-	return PRIMITIVE_TYPE_ID_VOID;
-}
-
-bool lox_void_type::operator<(const lox_type& target) const
-{
-	return false;
-}
-
-bool lox_void_type::operator==(const lox_type& another) const
-{
-	return another.id() == PRIMITIVE_TYPE_ID_VOID;
-}
-
-bool lox_void_type::operator!=(const lox_type& another) const
-{
-	return another.id() != PRIMITIVE_TYPE_ID_VOID;
-}
